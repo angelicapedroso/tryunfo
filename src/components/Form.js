01bb5a391f9ad4ee45f2fc1157d826f3dbce495c
Angelica <angelicapedroso05@gmail.com>
2021-11-15 17:37:02 -0300

    corrigindo erros

--- src/components/Form.js
+++ src/components/Form.js
@@ -113,7 +113,6 @@ class Form extends Component {
               value={ cardRare }
               onChange={ onInputChange }
             >
-              <option>Selecione a raridade</option>
               <option value="normal">normal</option>
               <option value="raro">raro</option>
               <option value="muito raro">muito raro</option>
